719a9df7934149ac70031fa97e7a544d401dce97
jiggypete <jiggypete@gmail.com>
2020-05-15 15:49:07 +0100

    Deleting scripts/

--- scripts/nigel-gatherer/list_all_local_files.rb
+++
@@ -1,6 +0,0 @@
-# require 'file'
-
-terminal_output = `cd ../../music; find nigel-gatherer -name "*.mp3" -print && find nigel-gatherer -name "*.pdf" -print`
-output = terminal_output.gsub("nigel-gatherer/", "")
-
-File.write("local_files.txt", output)
