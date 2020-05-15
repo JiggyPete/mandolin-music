8f7d0e01a4f09b521fc405510b9b18ff8e1f9b13
jiggypete <jiggypete@gmail.com>
2020-05-15 15:54:01 +0100

    Removing unused js files

--- app/javascript/random_song.js
+++
@@ -1,5 +0,0 @@
-let randomSongElement = document.getElementsByClassName("random-song")[0]
-randomSongElement.addEventListener('click', function (event) {
-  var randomIndex = Math.floor(Math.random() * Math.floor(data.length))
-  suggestions.displayAndFocus( data[randomIndex] );
-});
